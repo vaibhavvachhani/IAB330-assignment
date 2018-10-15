8fd7411d9aa8e917a1498f7df982af767d515f66
Vaibhav Vachhani <vaibhav.vachhani@hotmail.com>
2018-10-15 17:57:52 +1000

    Polished home page and profile page Added report a pest page

--- fishy/fishy/fishy/Views/Home.xaml.cs
+++ fishy/fishy/fishy/Views/Home.xaml.cs
@@ -22,5 +22,10 @@ namespace fishy.Views
         {
             await Navigation.PushAsync(new Weather(), true);
         }
+
+        private async void Button_Clicked_1(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Report(), true);
+        }
     }
 }

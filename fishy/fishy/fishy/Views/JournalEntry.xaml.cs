8967e8769574713a65c2d3954e14bea476424b38
Khargan Powell <kharganp@gmail.com>
2018-10-19 21:14:21 +1000

    Create Journal Entry page
    basic journal entry, can take photo, doesn't save/go anywhere for now but  basic functions right now

--- fishy/fishy/fishy/Views/JournalEntry.xaml.cs
+++ fishy/fishy/fishy/Views/JournalEntry.xaml.cs
@@ -12,9 +12,19 @@ namespace fishy.Views
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class JournalEntry : ContentPage
 	{
-		public JournalEntry ()
+		public JournalEntry()
 		{
 			InitializeComponent ();
-		}
-	}
+
+            CameraButton.Clicked += CameraButton_Clicked;
+        }
+
+        private async void CameraButton_Clicked(object sender, EventArgs e)
+        {
+            var photo = await Plugin.Media.CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions() { });
+
+            if (photo != null)
+                PhotoImage.Source = ImageSource.FromStream(() => { return photo.GetStream(); });
+        }
+    }
 }
